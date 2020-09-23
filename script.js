61ff58ba2efb7bf95054008dd281d6ba44dcc9fb
tiffany t <iluvpups20@gmail.com>
2020-09-22 20:53:16 -0500

    Added game page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var styles = `
-    .startPage {
+let styles = `
+    .tfny-wrapper {
         position: absolute;
         z-index: 1;
         top: 0;
@@ -11,49 +11,93 @@ var styles = `
         align-items: center;
         background: #4f9cd6;
         color: #fff;
-        font-family:"Helvetica Neue",Helvetica,"Lucida Grande","Lucida Sans Unicode",Arial,Verdana,sans-serif
+        font-family:"Helvetica Neue",Helvetica,"Lucida Grande","Lucida Sans Unicode",Arial,Verdana,sans-serif;
+        cursor: pointer;
     }
-    .startContainer {
+    .tfny-container {
         padding: 1rem;
     }
-    h1 {
+    .tfny-h1 {
         text-align: center;
         font-size: 4rem;
         margin: 0
     }
-    h2 {
+    .tfny-h2 {
         text-align: center;
         font-size: 1.3rem;
     }
+    .tfny-gameWrapper {
+        position: absolute;
+        z-index: 1;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        display: none;
+        justify-content: center;
+        align-items: center;
+        background: #000;
+        color: #fff;
+        font-family:"Helvetica Neue",Helvetica,"Lucida Grande","Lucida Sans Unicode",Arial,Verdana,sans-serif;
+    }
+    .tfny-circle {
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+        background: #f20c0c;
+        box-shadow: 0 2rem rgba(0, 0, 0, 0.3);
+    }
 `
 
-var styleSheet = document.createElement("style");
+let styleSheet = document.createElement("style");
 styleSheet.type = "text/css";
 styleSheet.innerText = styles;
 document.head.appendChild(styleSheet);
 
-function startReactionTest(wrapper) {
+function startReactionTest(canvas) {
     // create start page
-    var startPage = document.createElement("section");
-    startPage.classList.add("startPage");
+    let startPage = document.createElement("section");
+    startPage.classList.add("tfny-wrapper");
 
-        // create start page wrapper
-        var startContainer = document.createElement("div");
-        startContainer.classList.add("startContainer");
+        // create start page container
+        let startContainer = document.createElement("div");
+        startContainer.classList.add("tfny-container");
         startPage.appendChild(startContainer);
         // create title
-        var h1 = document.createElement("h1");
+        let h1 = document.createElement("h1");
+        h1.classList.add("tfny-h1");
         h1.appendChild(document.createTextNode("Reaction Time Test"));
         startContainer.appendChild(h1);
         // create subtitle
-        var h2 = document.createElement("h2");
+        let h2 = document.createElement("h2");
+        h2.classList.add("tfny-h2");
         h2.appendChild(document.createTextNode("When the red circle turns green, click/tap it as fast as possible. Click anywhere to begin!"));
         startContainer.appendChild(h2);
 
-    // append content to wrapper
-    wrapper.appendChild(startPage);
+    // create game canvas
+    let gamePage = document.createElement("section");
+    gamePage.classList.add("tfny-gameWrapper");
+
+        // create game page container
+        let gameContainer = document.createElement("div");
+        gameContainer.classList.add("tfny-container");
+        gamePage.appendChild(gameContainer);
+        // create circle
+        let circle = document.createElement('div');
+        circle.classList.add("tfny-circle");
+        gameContainer.appendChild(circle);
+
+    // append content to canvas
+    canvas.appendChild(startPage);
+    canvas.appendChild(gamePage);
+    
+    // on click of start page, start game
+    startPage.addEventListener('click', () => {
+        startPage.style.display = 'none';
+        gamePage.style.display = 'flex';
+    })
 }
 
 // call start test function on button click
 const startButton = document.getElementById("start");
-startButton.addEventListener('click', () => startReactionTest(document.getElementById("reactionTestWrapper")))
+startButton.addEventListener('click', () => startReactionTest(document.getElementById("tfny-canvas")))
