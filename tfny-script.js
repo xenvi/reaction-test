b003e51b098159a76c0f28721c78583b500ece39
tiffany t <iluvpups20@gmail.com>
2020-09-23 20:17:14 -0600

    Added timeout for circle color change, created error page

--- tfny-script.js
+++ tfny-script.js
@@ -1,7 +1,7 @@
 function startReactionTest(canvas) {
     // load files
     let styles = `
-        @keyframes fadein {
+        @keyframes tfny-fadein {
             from {
             opacity: 0;
             }
@@ -9,7 +9,7 @@ function startReactionTest(canvas) {
             opacity: 1;
             }
         }
-        @keyframes fadeout {
+        @keyframes tfny-fadeout {
             from {
             opacity: 1;
             }
@@ -31,7 +31,7 @@ function startReactionTest(canvas) {
             color: #fff;
             font-family:"Helvetica Neue",Helvetica,"Lucida Grande","Lucida Sans Unicode",Arial,Verdana,sans-serif;
             cursor: pointer;
-            animation: 0.2s linear fadein;
+            animation: 0.2s linear tfny-fadein;
         }
         .tfny-container {
             padding: 1rem;
@@ -59,13 +59,13 @@ function startReactionTest(canvas) {
             background: #4f9cd6;
             color: #fff;
             font-family:"Helvetica Neue",Helvetica,"Lucida Grande","Lucida Sans Unicode",Arial,Verdana,sans-serif;
-            animation: 0.2s linear fadein;
+            animation: 0.2s linear tfny-fadein;
         }
         .tfny-gameText {
             color: #fff;
             text-align: center;
         }
-        .tfny-circle {
+        .tfny-circleRed {
             width: 200px;
             height: 200px;
             border-radius: 50%;
@@ -73,10 +73,36 @@ function startReactionTest(canvas) {
             box-shadow: 0 2rem 1rem rgba(0, 0, 0, 0.3);
             cursor: pointer;
         }
+        .tfny-circleGreen {
+            width: 200px;
+            height: 200px;
+            border-radius: 50%;
+            background: #0cf264;
+            box-shadow: 0 2rem 1rem rgba(0, 0, 0, 0.3);
+            cursor: pointer;
+        }
+        .tfny-errorWrapper {
+            position: absolute;
+            z-index: 1;
+            top: 0;
+            left: 0;
+            width: 100vw;
+            height: 100vh;
+            display: none;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background: #4f9cd6;
+            color: #fff;
+            cursor: pointer;
+            font-family:"Helvetica Neue",Helvetica,"Lucida Grande","Lucida Sans Unicode",Arial,Verdana,sans-serif;
+            animation: 0.2s linear tfny-fadein;
+        }
     `
     let styleSheet = document.createElement("style");
     styleSheet.type = "text/css";
     styleSheet.innerText = styles;
+    document.head.appendChild(styleSheet);
 
     // create start page
     let startPage = document.createElement("section");
@@ -107,6 +133,10 @@ function startReactionTest(canvas) {
     })
 
     function gameFunction(canvas) {
+        // set variables
+        let roundNumber = 0;
+        let roundDataArr = [];
+
         // create game canvas
         let gamePage = document.createElement("section");
         gamePage.classList.add("tfny-gameWrapper");
@@ -122,16 +152,57 @@ function startReactionTest(canvas) {
             gamePage.appendChild(gameContainer);
             // create circle
             let circle = document.createElement('div');
-            circle.classList.add("tfny-circle");
+            circle.classList.add("tfny-circleRed");
             gameContainer.appendChild(circle);
+        
+        // create error canvas when click too fast
+        let errorPage = document.createElement("section");
+        errorPage.classList.add("tfny-errorWrapper");
+
+            // create title
+            let h1 = document.createElement("h1");
+            h1.classList.add("tfny-h1");
+            h1.appendChild(document.createTextNode("Oops! Too soon."));
+            errorPage.appendChild(h1);
+            // create subtitle
+            let h2 = document.createElement("h2");
+            h2.classList.add("tfny-h2");
+            h2.appendChild(document.createTextNode("Click to try again."));
+            errorPage.appendChild(h2);
     
         // append content to canvas
         canvas.appendChild(gamePage);
         gamePage.style.display = 'flex';
-    
-        // set variables
         
         // create timer
+        var rand = Math.floor(Math.random() * 3000) + 2000  // 2-3 secs
+        function changeButton() {
+            circle.classList.add("tfny-circleGreen");
+            circle.classList.remove("tfny-circleRed");
+        }
+        setTimeout(changeButton, rand);
+
+       // handle circle click
+       circle.addEventListener('click', () => {
+           if (circle.classList.contains('tfny-circleRed')) {
+                canvas.appendChild(errorPage);
+                errorPage.style.display = 'flex';
+           //} else if (circle.classList.contains('tfny-circleGreen')) {
+             //   canvas.appendChild(resultPage);
+               // resultPage.style.display = 'flex';
+            }
+       })
+
+       // on error page click, restart game
+       circle.addEventListener('click', () => {
+        if (circle.classList.contains('tfny-circleRed')) {
+             canvas.appendChild(errorPage);
+             errorPage.style.display = 'flex';
+        //} else if (circle.classList.contains('tfny-circleGreen')) {
+          //   canvas.appendChild(resultPage);
+            // resultPage.style.display = 'flex';
+         }
+    })
     }
 }
 
